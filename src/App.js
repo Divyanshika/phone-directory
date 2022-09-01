8e98abd220c0e15619688773c0483e0006662ae9
Divyanshika <nityanshi99.mishr@gmail.com>
2022-09-01 22:06:31 +0530

    Added 'Delete' button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
+import './App.css';
 class App extends Component{
   render() {
+    let subscribers=[
+      {
+        id:1,
+        Name:"Shilpa",
+        Phone:999999999,
+      },
+      {
+        id:2,
+        Name:"Shipra",
+        Phone:9998888999,
+      }
+    ];
     return(
       <div>
          <Header/>
-          <button>Add</button>
-          <div>
-            <span>Name</span><br/>
-            <span>Phone</span>
-          </div>
+         <div className='component-body-container'>
+          <button className='custom-btn add-btn'>Add</button>
+          <div className='grid-container heading-container'>
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+           </div >
+           
+          {
+            subscribers.map(sub=>{
+              return <div key={sub.id} className='grid-container'>
+              <span className="grid-item name-heading">{sub.Name}</span>
+                <span className="grid-item phone-heading">{sub.Phone}</span>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+                </div>
+            })
+          }
+         </div>
       </div>
     );
   }
