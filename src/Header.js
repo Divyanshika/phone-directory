a7fa173ae534f263447306fe59d95578d78103a4
Divyanshika <nityanshi99.mishr@gmail.com>
2022-09-05 20:37:14 +0530

    Passed heading to Header component as props

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ class Header extends Component{
         const headerStyle={textAlign:'center', padding:20 , background:'#000' , color:'#fff', textTransform:'uppercase'}
         return(
             <div style={headerStyle}>
-                Phone Directory
+                {this.props.heading}
             </div>
         )
     }
